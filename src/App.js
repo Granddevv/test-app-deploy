721c97bed496adf64db41a107302d1aff243fb7c
granddevv <spacingflight@outlook.com>
2020-11-06 10:40:20 +0100

    update pipeline

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is deloyed to Firebase with Github CI/CD
         </a>
       </header>
     </div>

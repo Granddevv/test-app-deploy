8409f3475c13d8955bd760e0fceac974037ff0af
granddevv <spacingflight@outlook.com>
2020-11-20 18:08:01 -0800

    Add Interest section

---
+++ src/containers/Interest/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './style.scss';
+
+const Interest = () => {
+    return (
+        <div className="section-interest">
+            <div className="section-wrapper">
+                <h1>INTEREST</h1>
+                <div className="interest-container">
+                    <p>soslsdfsf good iing ig ig idigididiiididiiidididiididididiiiid</p>
+                    <div className="items-container">
+                        <div className="item-wrapper">
+                            <h6>Sports</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Interest;

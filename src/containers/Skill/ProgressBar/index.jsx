a3347429139890fc03d874c8a73efe2bfee202ab
granddevv <spacingflight@outlook.com>
2020-11-20 18:09:50 -0800

    Add ProgressBar

---
+++ src/containers/Skill/ProgressBar/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './style.scss';
+
+const ProgressBar = ({
+    title,
+    value
+                     }) => {
+    return (
+        <div className="progress-container">
+            <div className="title-container">
+                <h4>{title}</h4>
+                <h4>{value}%</h4>
+            </div>
+            <div className="bar-container">
+                <div className="bar-point" style={{left: `${value}%`}}></div>
+                <div className="bar-content"></div>
+                <div className="bar-content-fill" style={{width: `${value}%`}}></div>
+            </div>
+        </div>
+    )
+}
+
+export default ProgressBar;

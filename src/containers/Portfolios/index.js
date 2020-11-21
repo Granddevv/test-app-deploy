f71c4ff1fb61cda1d09e89e24d4d38639f25c8de
granddevv <spacingflight@outlook.com>
2020-11-20 18:08:48 -0800

    Add Portfolio Section

---
+++ src/containers/Portfolios/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './style.scss'
+
+const Portfolios = () => {
+    return (
+        <div className="section-portfolio">
+            <div className="section-wrapper">
+                <h1>PORTFOLIOS</h1>
+                <div className="portfolio-section">
+                    <div className="item-container">
+                        <div className="image-container"></div>
+                        <div className="title-container">Portfolio Title Hero</div>
+                    </div>
+                    <div className="item-container">
+                        <div className="image-container"></div>
+                        <div className="title-container">Portfolio Title Hero</div>
+                    </div>
+                    <div className="item-container">
+                        <div className="image-container"></div>
+                        <div className="title-container">Portfolio Title Hero</div>
+                    </div>
+                    <div className="item-container">
+                        <div className="image-container"></div>
+                        <div className="title-container">Portfolio Title Hero</div>
+                    </div>
+                    <div className="item-container">
+                        <div className="image-container"></div>
+                        <div className="title-container">Portfolio Title Hero</div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Portfolios

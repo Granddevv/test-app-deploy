16e12ad39bc3ee7bbd4edda350740ac23071636b
granddevv <spacingflight@outlook.com>
2020-11-20 18:06:30 -0800

    Add Archivement

---
+++ src/containers/Archievement/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './style.scss'
+
+const Archievement = () => {
+    return (
+        <div className="section-archievement">
+            <div className="section-wrapper">
+                <h1>ARCHIVEMENT</h1>
+                <div className="archieve-container">
+                    <div className="item-container">
+                        <div className="title-container">
+                            <h3>ACM SOFTWARE SYSTEM AWARD</h3>
+                        </div>
+                        <div className="content-container">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Archievement

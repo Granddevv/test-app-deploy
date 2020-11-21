78ec222c903ec82226e35caac17139a1b2fc7041
granddevv <spacingflight@outlook.com>
2020-11-20 18:04:54 -0800

    Add const values

---
+++ src/const/cloudinary.js
@@ -0,0 +1,9 @@
+// cloudinary.uploader.upload("sample.jpg", {"crop":"limit","tags":"samples","width":3000,"height":2000}, function(result) { console.log(result) });
+
+const cloudinary = () => {
+    return {
+
+    }
+}
+
+export { cloudinary }
---
+++ src/const/profile_infor.js
@@ -0,0 +1,154 @@
+const profileInfor = [
+    {
+        key: "d8f78sf9ds8dfs9f7s97sfs9fs9",
+        firstName: 'Jack',
+        lastName: 'Smith',
+        title: 'Full Stack React & Node Developer',
+        profileUrl: 'https://res.cloudinary.com/dzz64onxh/image/upload/v1605864670/react_vbpsq7.png',
+        contactOption: {
+            linkedin: 'https://www.linkedin.com/in/anthony-wong-500b15143/',
+            phone: '192923823'
+        },
+        overview: "Hi, This is Anthony. I have been working with various project for over 8 years and here are looking soso as a senior developer, I have rich experience in building web applications using React and Redux.",
+        workHistory: [
+            {
+                title: 'Senior Frontend Developer',
+                companyName: 'Rapticore',
+                country: 'US',
+                period: 'January 2015 - February 2017',
+                description: "I've worked on 3 main products including SPA, Landing Page, PWA. Built the initial products very quickly and upgraded its version to 2.0, 3.0. Customized the whole layout and converted"
+            },
+            {
+                title: 'Senior Frontend Developer',
+                companyName: 'Rapticore',
+                country: 'US',
+                period: 'January 2015 - February 2017',
+                description: "I've worked on 3 main products including SPA, Landing Page, PWA. Built the initial products very quickly and upgraded its version to 2.0, 3.0. Customized the whole layout and converted"
+            },
+            {
+                title: 'Senior Frontend Developer',
+                companyName: 'Rapticore',
+                country: 'US',
+                period: 'January 2015 - February 2017',
+                description: "I've worked on 3 main products including SPA, Landing Page, PWA. Built the initial products very quickly and upgraded its version to 2.0, 3.0. Customized the whole layout and converted"
+            },
+            {
+                title: 'Senior Frontend Developer',
+                companyName: 'Rapticore',
+                country: 'US',
+                period: 'January 2015 - February 2017',
+                description: "I've worked on 3 main products including SPA, Landing Page, PWA. Built the initial products very quickly and upgraded its version to 2.0, 3.0. Customized the whole layout and converted"
+            }
+        ],
+        skill: [
+            {
+                title: 'Professional',
+                skillItem: [
+                    {
+                        key: 'UI/UX Design',
+                        value: 90
+                    },
+                    {
+                        key: 'UI/UX Design',
+                        value: 90
+                    },
+                    {
+                        key: 'UI/UX Design',
+                        value: 90
+                    },
+                    {
+                        key: 'UI/UX Design',
+                        value: 90
+                    },
+                    {
+                        key: 'UI/UX Design',
+                        value: 90
+                    }
+                ]
+            },
+            {
+                title: 'Personal',
+                skillItem: [
+                    {
+                        key: 'UI/UX Design',
+                        value: 90
+                    },
+                    {
+                        key: 'UI/UX Design',
+                        value: 90
+                    },
+                    {
+                        key: 'UI/UX Design',
+                        value: 90
+                    },
+                    {
+                        key: 'UI/UX Design',
+                        value: 90
+                    },
+                    {
+                        key: 'UI/UX Design',
+                        value: 90
+                    }
+                ]
+            },
+            {
+                title: 'Language',
+                skillItem: [
+                    {
+                        key: 'UI/UX Design',
+                        value: 90
+                    },
+                    {
+                        key: 'UI/UX Design',
+                        value: 90
+                    },
+                    {
+                        key: 'UI/UX Design',
+                        value: 90
+                    },
+                    {
+                        key: 'UI/UX Design',
+                        value: 90
+                    },
+                    {
+                        key: 'UI/UX Design',
+                        value: 90
+                    }
+                ]
+            }
+
+        ],
+        education: [
+            {
+                title: 'Senior Frontend Developer',
+                companyName: 'Rapticore',
+                country: 'US',
+                period: 'January 2015 - February 2017',
+                description: "I've worked on 3 main products including SPA, Landing Page, PWA. Built the initial products very quickly and upgraded its version to 2.0, 3.0. Customized the whole layout and converted"
+            },
+            {
+                title: 'Senior Frontend Developer',
+                companyName: 'Rapticore',
+                country: 'US',
+                period: 'January 2015 - February 2017',
+                description: "I've worked on 3 main products including SPA, Landing Page, PWA. Built the initial products very quickly and upgraded its version to 2.0, 3.0. Customized the whole layout and converted"
+            },
+            {
+                title: 'Senior Frontend Developer',
+                companyName: 'Rapticore',
+                country: 'US',
+                period: 'January 2015 - February 2017',
+                description: "I've worked on 3 main products including SPA, Landing Page, PWA. Built the initial products very quickly and upgraded its version to 2.0, 3.0. Customized the whole layout and converted"
+            },
+            {
+                title: 'Senior Frontend Developer',
+                companyName: 'Rapticore',
+                country: 'US',
+                period: 'January 2015 - February 2017',
+                description: "I've worked on 3 main products including SPA, Landing Page, PWA. Built the initial products very quickly and upgraded its version to 2.0, 3.0. Customized the whole layout and converted"
+            }
+        ]
+    }
+]
+
+export { profileInfor }

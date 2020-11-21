34b59be906673c95651a89fcf8ca4d4316b80833
granddevv <spacingflight@outlook.com>
2020-11-20 18:02:45 -0800

    Update App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import './bootrstrap.css'
+import MainPage from './containers';
+import Spinner from './components/Spinner';
+import {
+    BrowserRouter as Router
+} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          This is deloyed to Firebase with Github CI/CD
-        </a>
-      </header>
+        <Router>
+            <MainPage/>
+        </Router>
     </div>
   );
 }

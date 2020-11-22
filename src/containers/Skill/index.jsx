b7fd00bc2a83e90c23dc734dfc351c0cd16ea688
granddevv <spacingflight@outlook.com>
2020-11-21 18:10:22 -0800

    Add Skill Section

---
+++ src/containers/Skill/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './style.scss';
+import ProgressBar from './ProgressBar';
+
+const Skill = ({
+       skillOption
+               }) => {
+    return (
+        <div className="section-skill">
+            <div className="skill-section-wrapper">
+                <h1>SKILLS</h1>
+                <div className="skill-container">
+                    {skillOption.map(skillItem => (
+                        <div className="skill-panel">
+                            <h3>{skillItem.title}</h3>
+                            {skillItem.skillItem.map(item => (
+                                <ProgressBar
+                                    title={item.key}
+                                    value={item.value}
+                                />
+                            ))}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Skill

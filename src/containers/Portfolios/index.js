6a9f5393c7d206439bf8b6271d26acabd6d7b4f8
granddevv <spacingflight@outlook.com>
2020-11-26 02:19:09 -0800

    Portfolio section

--- src/containers/Portfolios/index.js
+++ src/containers/Portfolios/index.js
@@ -1,33 +1,24 @@
 import React from 'react';
 import './style.scss'
 
-const Portfolios = () => {
+const Portfolios = ({
+    portfolios
+                    }) => {
     return (
         <div className="section-portfolio">
             <div className="section-wrapper">
                 <h1>PORTFOLIOS</h1>
                 <div className="portfolio-section">
-                    <div className="item-container">
-                        <div className="image-container"></div>
-                        <div className="title-container">Portfolio Title Hero</div>
-                    </div>
-                    <div className="item-container">
-                        <div className="image-container"></div>
-                        <div className="title-container">Portfolio Title Hero</div>
-                    </div>
-                    <div className="item-container">
-                        <div className="image-container"></div>
-                        <div className="title-container">Portfolio Title Hero</div>
-                    </div>
-                    <div className="item-container">
-                        <div className="image-container"></div>
-                        <div className="title-container">Portfolio Title Hero</div>
-                    </div>
-                    <div className="item-container">
-                        <div className="image-container"></div>
-                        <div className="title-container">Portfolio Title Hero</div>
-                    </div>
-
+                    {portfolios.map(portfolioItem => (
+                        <div className="item-container">
+                            <div className="image-container" style={{backgroundImage: `url("${portfolioItem.url}")`}}>
+                                <div className="detail-option">
+                                    <span>View Details</span>
+                                </div>
+                            </div>
+                            <div className="title-container">{portfolioItem.title}</div>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>

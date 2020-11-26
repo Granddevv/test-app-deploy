0d7b79a256b58ab516e9295fd55220dc071ee7ed
granddevv <spacingflight@outlook.com>
2020-11-26 02:20:58 -0800

    Add archivement

--- src/containers/Archievement/index.jsx
+++ src/containers/Archievement/index.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import './style.scss'
 
-const Archievement = () => {
+const Archievement = ({
+                          certifications
+                      }) => {
     return (
         <div className="section-archievement">
             <div className="section-wrapper">
                 <h1>ARCHIVEMENT</h1>
                 <div className="archieve-container">
-                    <div className="item-container">
-                        <div className="title-container">
-                            <h3>ACM SOFTWARE SYSTEM AWARD</h3>
+                    {certifications.map(certification => (
+                        <div className="item-container">
+                            <div className="title-container">
+                                <h3>{certification.title}</h3>
+                            </div>
+                            <div className="content-container">
+                                <img className="cert-logo" src={certification.url} />
+                            </div>
                         </div>
-                        <div className="content-container">
-
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </div>
         </div>

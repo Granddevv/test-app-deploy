694184cfc59f3a1df88f96db52295f4f2a50c9dc
granddevv <spacingflight@outlook.com>
2020-12-02 12:16:48 -0800

    Fix profile section

--- src/const/profile_infor.js
+++ src/const/profile_infor.js
@@ -235,6 +235,221 @@ const profileInfor = [
                 }
             ]
         }
+    },
+    {
+        key: "4388ff16-34b6-11eb-adc1-0242ac120002",
+        firstName: 'Marc',
+        lastName: 'Liu',
+        title: 'Full Stack React/React Native & Node Developer',
+        profileUrl: 'https://res.cloudinary.com/dzz64onxh/image/upload/v1606924673/photo_kdtfbp.jpg',
+        contactOption: {
+            linkedin: 'https://www.linkedin.com/in/marc-liu-5415b019b/',
+            phone: '+1 (343) 520-1427',
+            address: "Alexandria, Ontario, Canada",
+            email: 'marcliuprofess@gmail.com',
+            name: 'Marc Liu'
+        },
+        overview: "Marc Liu is a full-stack web developer with 11 years dedicated experience in building web & native applications using JavaScript and JavaScript frameworks - React.JS, Angular and Node.JS. He has been working with various kinds of projects including SPAs, PWAs, Native APPs, Hybrid APPs and always being focused on delivering well designed products.",
+        workHistory: [
+            {
+                title: 'Web Developer',
+                companyName: 'Self Employed',
+                country: 'Canada',
+                period: 'Apr 2020 - ',
+                description: "I worked on 2 short term projects as a freelance developer because of COVID-19. One was building native application and dashboard application using React, React Native. The another one was building real time data post application using React & Ionic."
+            },
+            {
+                title: 'Full Stack Web & Mobile Developer',
+                companyName: 'Memory',
+                country: 'Oslo, Norway',
+                period: 'Oct 2019 - Apr 2020',
+                description: "I worked on time tracking application and dashboard UI using Angular, Ionic."
+            },
+            {
+                title: 'Full Stack Web Developer',
+                companyName: 'Recart',
+                country: 'Budapest, Hungary',
+                period: 'Jul 2018 - Aug 2019',
+                description: "I worked on building shopify facebook messenger application using React.JS."
+            },
+            {
+                title: 'Senior Frontend Developer',
+                companyName: 'Izenda',
+                country: 'Atlanta, United States',
+                period: 'Apr 2017 - Sep 2018',
+                description: "Worked on building dashboard data analyze application using React.JS & Mobx"
+            }
+        ],
+        skill: [
+            {
+                title: 'Frontend Development',
+                skillItem: [
+                    {
+                        key: 'HTML/HTML5, CSS/CSS3/SCSS/LESS',
+                        value: 100
+                    },
+                    {
+                        key: 'JavaScript & TypeScript',
+                        value: 95
+                    },
+                    {
+                        key: 'React.JS & Redux, Mobx',
+                        value: 100
+                    },
+                    {
+                        key: 'Angular & RxJS',
+                        value: 100
+                    },
+                    {
+                        key: 'Vue.JS & Vuex & Vuetify',
+                        value: 90
+                    }
+                ]
+            },
+            {
+                title: 'Backend Development',
+                skillItem: [
+                    {
+                        key: 'Node.JS & Express.JS',
+                        value: 100
+                    },
+                    {
+                        key: 'MongoDB/Mongoose & MySQL',
+                        value: 100
+                    },
+                    {
+                        key: 'GraphQL Server',
+                        value: 95
+                    },
+                    {
+                        key: 'Linux & Ubuntu',
+                        value: 90
+                    },
+                    {
+                        key: 'Nginx, Apache',
+                        value: 90
+                    }
+                ]
+            },
+            {
+                title: 'Third Party Services',
+                skillItem: [
+                    {
+                        key: 'AWS/EC2/Lambda/S3/VPC/Elastic Beanstalk',
+                        value: 90
+                    },
+                    {
+                        key: 'Heroku, Digital Ocean',
+                        value: 100
+                    },
+                    {
+                        key: 'Firebase & Google Cloud Function',
+                        value: 100
+                    },
+                    {
+                        key: 'Docker & Docker Compose',
+                        value: 100
+                    },
+                    {
+                        key: 'Architecture',
+                        value: 90
+                    }
+                ]
+            }
+        ],
+        education: [
+            {
+                title: 'National University of Singapore',
+                degree: "Bachelor's degree",
+                country: 'Singapore',
+                period: 'January 2015 - February 2017',
+                description: "Computer Science"
+            }
+        ],
+        services: [
+            {
+                title: 'Web Frontend Development',
+                description: '',
+                icon: 'https://res.cloudinary.com/dzz64onxh/image/upload/v1606219243/download_1_l8b7f9.png'
+            },
+            {
+                title: 'Web Backend Development',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi venenatis et tortor ac tincidunt. In euismod iaculis lobortis. Vestibulum posuere molestie ipsum vel. Vestibulum venenatis pharetra mi, ut vestibulum elit ultricies.',
+                icon: 'https://res.cloudinary.com/dzz64onxh/image/upload/v1606219243/download-1_fefdzq.png'
+            },
+            {
+                title: 'Native App Application',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi venenatis et tortor ac tincidunt. In euismod iaculis lobortis. Vestibulum posuere molestie ipsum vel. Vestibulum venenatis pharetra mi, ut vestibulum elit ultricies.',
+                icon: 'https://res.cloudinary.com/dzz64onxh/image/upload/v1606219243/download_yoquc7.png'
+            },
+
+        ],
+        portfolios: [
+            {
+                title: "Izenda UI Development - React",
+                description: "Izenda is a managmenet system. This is based on React frontend and C# & ASP.NET backend. I worked on this project as a frontend developer and built the whole UI and integrated",
+                url: "https://res.cloudinary.com/dzz64onxh/image/upload/v1606381357/20_zapcvv.png"
+            },
+            {
+                title: "Izenda UI Development - React",
+                description: "Izenda is a managmenet system. This is based on React frontend and C# & ASP.NET backend. I worked on this project as a frontend developer and built the whole UI and integrated",
+                url: "https://res.cloudinary.com/dzz64onxh/image/upload/v1606381357/20_zapcvv.png"
+            },
+            {
+                title: "Izenda UI Development - React",
+                description: "Izenda is a managmenet system. This is based on React frontend and C# & ASP.NET backend. I worked on this project as a frontend developer and built the whole UI and integrated",
+                url: "https://res.cloudinary.com/dzz64onxh/image/upload/v1606381357/20_zapcvv.png"
+            },
+            {
+                title: "Izenda UI Development - React",
+                description: "Izenda is a managmenet system. This is based on React frontend and C# & ASP.NET backend. I worked on this project as a frontend developer and built the whole UI and integrated",
+                url: "https://res.cloudinary.com/dzz64onxh/image/upload/v1606381357/20_zapcvv.png"
+            }
+        ],
+        certifications: [
+            {
+                title: "JavaScript Certification",
+                url: "https://res.cloudinary.com/dzz64onxh/image/upload/v1606383221/testdome_logo_fvs42d.png"
+            },
+            {
+                title: "React.JS Certification",
+                url: "https://res.cloudinary.com/dzz64onxh/image/upload/v1606383221/testdome_logo_fvs42d.png"
+            },
+            {
+                title: "Node.JS Certification",
+                url: "https://res.cloudinary.com/dzz64onxh/image/upload/v1606383221/testdome_logo_fvs42d.png"
+            },
+
+        ],
+        interests: {
+            title: 'I like music and play the guitar when I have some free time. Football is my favorite sports and I like playing with my friends.',
+            options: [
+                {
+                    title: 'Football',
+                    iconName: 'ion-ios-football'
+                },
+                {
+                    title: 'Football',
+                    iconName: 'ion-ios-football'
+                },
+                {
+                    title: 'Football',
+                    iconName: 'ion-ios-football'
+                },
+                {
+                    title: 'Football',
+                    iconName: 'ion-ios-football'
+                },
+                {
+                    title: 'Football',
+                    iconName: 'ion-ios-football'
+                },
+                {
+                    title: 'Football',
+                    iconName: 'ion-ios-football'
+                }
+            ]
+        }
     }
 ]
 
